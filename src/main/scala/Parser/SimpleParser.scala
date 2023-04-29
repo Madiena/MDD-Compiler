c297e544799d108ee676bf223abafffcfdbb0611
Madiena <madelainehestermann@yahoo.de>
2023-04-29 17:45:15 +0200

    Add training parser

---
+++ src/main/scala/Parser/SimpleParser.scala
@@ -0,0 +1,12 @@
+package Parser
+import scala.util.parsing.combinator._
+
+class SimpleParser extends RegexParsers {
+  def word: Parser[String] = """[a-z]+""".r ^^ {_.toString}
+  def number: Parser[Int] = """(0|[1-9]\d*)""".r ^^ {_.toInt}
+  def freq: Parser[WordFreq] = word ~ number ^^ {case wd ~ fr => WordFreq(wd, fr)}
+}
+
+case class WordFreq(word: String, count: Int) {
+  override def toString = "Word <" + word + ">" + "occurs with frequency" + count
+}
