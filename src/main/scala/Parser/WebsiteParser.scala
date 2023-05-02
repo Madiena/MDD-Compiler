2edb09222100eea2868a745db3aea39e7bd90443
Madiena <madelainehestermann@yahoo.de>
2023-05-02 11:52:55 +0200

    Add Abstract Syntax Tree and WebsiteParser

---
+++ src/main/scala/Parser/WebsiteParser.scala
@@ -0,0 +1,31 @@
+package Parser
+
+import scala.util.parsing.combinator.RegexParsers
+
+class WebsiteParser extends RegexParsers{
+
+  def identifier:
+  def destination:
+  def table:
+  def link:
+  def textArea:
+  def input:
+  def label:
+  def formular:
+  def paragraph:
+  def headline:
+  def text:
+  def icon:
+  def listElement:
+  def list:
+  def image:
+  def footer:
+  def navbar:
+  def image:
+  def header:
+  def body:
+  def page:
+  def website:
+
+
+}
