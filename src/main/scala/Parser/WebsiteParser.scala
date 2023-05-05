e314328d7d6463d8c0dce8a834b1f26d6fb5082b
Madiena <madelainehestermann@yahoo.de>
2023-05-05 10:01:46 +0200

    Work on M&M's super Website language grammar

--- src/main/scala/Parser/WebsiteParser.scala
+++ src/main/scala/Parser/WebsiteParser.scala
@@ -5,14 +5,24 @@ import scala.util.parsing.combinator.RegexParsers
 class WebsiteParser extends RegexParsers{
 
   def word: Parser[String] = """([a-z]+)|([A-Z]+)|(0|[1-9]\d*)""".r ^^ {_.toString}
-  def identifier: Parser[String] = """( |/|-|!|:|,|&)""".r | word ^^ {_.toString}
-  def destination: Parser[String] = word ~ """(.html)""".r ^^ {_.toString()}
-  def tablehead: Parser[String] = word ^^ {_.toString()}
-  def tabledata: Parser[String] = identifier ^^ {_.toString()}
+  def identifierWord: Parser[String] = """([ /\-!:,&)]*)""".r | word ^^ {_.toString}
 
-  def tablerowlist: Parser[] = tablerowlist ~ tablerow
-  def tablerow: Parser[String]  = tablehead | tabledata
-  def table:
+  // Destination = (destination)
+  def destination: Parser[String] = """\(""".r ~ word ~ """\((.html)\),""".r ~ """\)""".r ^^ {_.toString()}
+  // Tablehead = (tablehead),
+  def tablehead: Parser[String] = """\(""".r ~ word ~ """\),""".r ^^ {_.toString()}
+  // Tableheadend = (tablehead)
+  def tableheadend: Parser[String] = """\(""".r ~ word ~ """\)""".r ^^ {_.toString()}
+  // Tabledata = (tabledata),
+  def tabledata: Parser[String] = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
+  // TODO: tabledataend, like tabledata but w/ trailing comma
+  // Tablerow = (tablerow: <tabledata+ | tablehead+>), TODO: Check whether tabledata and tablehead can be mixed or not
+  def tablerow  = """\(Tablerow:""".r ~ (rep1(tablehead) | rep1(tabledata)) ~ """\),""".r  //TODO: function building AST node
+  // Table = (table: <tablerow+>)
+  def table = """\(Table:""".r  ~ rep1(tablerow) ~ """\),""".r //TODO: function building AST node
+  // Identifier = (identifier),
+  def identifier = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
+  // Link = (link: <identifer ~ destination>
   def link:
   def textArea:
   def input:
