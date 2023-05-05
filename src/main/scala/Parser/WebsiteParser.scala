543dbfcbc3f45820b2a4ceba60104b78ab4492ff
Melanie Hartmann <melanie.hartmann@ges.thm.de>
2023-05-05 12:27:08 +0200

    Gedanken und Kommentare zu den einzelnen Elementen

--- src/main/scala/Parser/WebsiteParser.scala
+++ src/main/scala/Parser/WebsiteParser.scala
@@ -22,25 +22,41 @@ class WebsiteParser extends RegexParsers{
   def table = """\(Table:""".r  ~ rep1(tablerow) ~ """\),""".r //TODO: function building AST node
   // Identifier = (identifier),
   def identifier = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
-  // Link = (link: <identifer ~ destination>
+  // Link = (link: <identifier ~ destination>)
   def link:
+  // textArea = (textarea),
   def textArea:
+  // input = (<label ~ input ~ placeholder>) // Vielleicht müssen die Styling-Tags auch hier mit rein später?! Placeholder ist ja so gesehen ein Teil des Inputs, aber programmiertechnisch ein Styling bzw eine Zusatzspezifikation des Input-Tags
   def input:
-  def label:
+  // label = (label)
+  def label: Parser[String] = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
+  // formular = (<label ~ input ~ placeholder>)
   def formular:
+  // paragraph = (paragraph)
   def paragraph:
+  // headline = (headline) // Hier ggf. mehrere für Unterscheidung h1-h4?
   def headline:
+  // text = (text)
   def text:
+  // icon = (icon)
   def icon:
+  // listelement = (listelement)
   def listElement:
+  // list = (listelement)*
   def list:
+  // image = (image)
   def image:
+  // footer = (footer: <link ~ link>)
   def footer:
+  // navbar = (navbar: <link*>) // ToDo: Überlegen, wie wir umsetzen dass Links auch "Unterlinks" haben können in der Navbar.. zwei Linkarten oder als optionales Attribut hinzufügen oder ganz anders?!
   def navbar:
-  def image:
+  // header = (header: <image ~ navbar>) // CAVE: Tatsächlich ist der header leer, logo befindet sich im Body.. aber sollte ja trotzdem gleich aussehen und irrelevant sein am Ende
   def header:
+  // body = (body: <image* ~ text* ~ list* ~ icon* ~ table* ~ formular* ~ link*>)
   def body:
+  // page = (page: <header ~ body ~ footer)
   def page:
+  // website = (website: <page*>)
   def website:
 
 
