a2be1e6c88c82a3c25e0c905ae01e8b4df39bb6e
Madiena <madelainehestermann@yahoo.de>
2023-05-05 12:55:53 +0200

    More work

--- src/main/scala/Parser/WebsiteParser.scala
+++ src/main/scala/Parser/WebsiteParser.scala
@@ -7,6 +7,13 @@ class WebsiteParser extends RegexParsers{
   def word: Parser[String] = """([a-z]+)|([A-Z]+)|(0|[1-9]\d*)""".r ^^ {_.toString}
   def identifierWord: Parser[String] = """([ /\-!:,&)]*)""".r | word ^^ {_.toString}
 
+  def statmentmiddleel = """\(""".r ~ id ~ """\),""".r
+
+  def statementendel = """\(""".r ~ id ~ """\)""".r
+  def statementlist = id ~ """:""".r ~ rep1(statementendel) ~ statementendel
+
+  def endel = destination | tablehead | tabledata | tablerow |
+
   // Destination = (destination)
   def destination: Parser[String] = """\(""".r ~ word ~ """\((.html)\),""".r ~ """\)""".r ^^ {_.toString()}
   // Tablehead = (tablehead),
@@ -15,11 +22,18 @@ class WebsiteParser extends RegexParsers{
   def tableheadend: Parser[String] = """\(""".r ~ word ~ """\)""".r ^^ {_.toString()}
   // Tabledata = (tabledata),
   def tabledata: Parser[String] = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
-  // TODO: tabledataend, like tabledata but w/ trailing comma
-  // Tablerow = (tablerow: <tabledata+ | tablehead+>), TODO: Check whether tabledata and tablehead can be mixed or not
-  def tablerow  = """\(Tablerow:""".r ~ (rep1(tablehead) | rep1(tabledata)) ~ """\),""".r  //TODO: function building AST node
+  // Tabledataend = (tabledata)
+  def tabledataend = """\(""".r ~ identifierWord ~ """\)""".r ^^ {_.toString()}
+  // Tablerowhead = (tablerow: <tablehead+>),
+  def tablerowhead  = """\(Tablerow:""".r ~ (rep1(tablehead) ~ tableheadend) ~ """\),""".r  //TODO: function building AST node
+  // Tablerowheadend = (tablerow: <tablehead+>)
+  def tablerowheadend  = """\(Tablerow:""".r ~ (rep1(tablehead) ~ tableheadend) ~ """\)""".r  //TODO: function building AST node
+  // Tablerowdata = (tablerow: <tabledata+>),
+  def tablerowdata  = """\(Tablerow:""".r ~ (rep1(tabledata) ~ tabledataend) ~ """\),""".r  //TODO: function building AST node
+  // Tablerowdataend = (tablerow: <tabledata+>)
+  def tablerowdataend  = """\(Tablerow:""".r ~ (rep1(tabledata) ~ tabledataend) ~ """\)""".r  //TODO: function building AST node
   // Table = (table: <tablerow+>)
-  def table = """\(Table:""".r  ~ rep1(tablerow) ~ """\),""".r //TODO: function building AST node
+  def table = """\(Table:""".r  ~ rep1(tablerowhead) ~ tablerowheadend ~ rep1(tablerowdata) ~ tablerowdataend ~ """\),""".r //TODO: function building AST node
   // Identifier = (identifier),
   def identifier = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
   // Link = (link: <identifer ~ destination>
