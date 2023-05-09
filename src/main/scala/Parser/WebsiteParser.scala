5f457585619dff2e122bf93f550163a9c885e496
Melanie Hartmann <melanie.hartmann@ges.thm.de>
2023-05-09 13:41:54 +0200

    added regex for elements

--- src/main/scala/Parser/WebsiteParser.scala
+++ src/main/scala/Parser/WebsiteParser.scala
@@ -9,7 +9,7 @@ class WebsiteParser extends RegexParsers{
   def statementist = statementbeg
   def statementbeg = (statementbegel ~ """:""".r ~ (rep(statementmiddleel) ~ statementendel) | statementbeg ) ^^ {_.toString}
 
-  def statementbegel =  tablerow | table
+  def statementbegel =  tablerow | table | link | textArea | paragraph | formular | headline | text | icon | listElement
   def statementendel: Parser[String] = """\(""".r ~ endel ~ """\)""".r ^^ {_.toString()}
 
   def statementmiddleel = """\(""".r ~ middleel ~ """\),""".r ^^ {_.toString()}
@@ -34,43 +34,43 @@ class WebsiteParser extends RegexParsers{
 
   def table: Parser[String] =  """Table""".r
 
-
   // Link = (link: <identifier ~ destination>)
- /* def link:
-  // textArea = (textarea),
-  def textArea:
+  def link: Parser[String] = """Link""".r
+  // textArea = (textarea)
+  def textArea: Parser[String] = """TextArea""".r
   // input = (<label ~ input ~ placeholder>) // Vielleicht müssen die Styling-Tags auch hier mit rein später?! Placeholder ist ja so gesehen ein Teil des Inputs, aber programmiertechnisch ein Styling bzw eine Zusatzspezifikation des Input-Tags
-  def input:
+  def input: Parser[String] = """\(""".r ~ label ~ input ~ placeholder ~ """\),""".r ^^ {_.toString()}
   // label = (label)
-  def label: Parser[String] = """\(""".r ~ identifierWord ~ """\),""".r ^^ {_.toString()}
+  def label: Parser[String] = """\(""".r ~ identifier ~ """\),""".r ^^ {_.toString()}
+ // placeholder = (placeholder)
+  def placeholder: Parser[String] = """Placeholder""".r
   // formular = (<label ~ input ~ placeholder>)
-  def formular:
+  def formular: Parser[String] = """Input""".r
   // paragraph = (paragraph)
-  def paragraph:
+  def paragraph: Parser[String] = """Paragraph""".r
   // headline = (headline) // Hier ggf. mehrere für Unterscheidung h1-h4?
-  def headline:
+  def headline: Parser[String] = """Headline""".r
   // text = (text)
-  def text:
+  def text: Parser[String] = """Text""".r
   // icon = (icon)
-  def icon:
+  def icon: Parser[String] = """Icon""".r
   // listelement = (listelement)
-  def listElement:
+  def listElement: Parser[String] = """ListElement""".r
   // list = (listelement)*
-  def list:
+  def list: Parser[String] = """\(""".r ~ listElement ~ """\*""".r ~ """\)""".r ^^ {_.toString()}
   // image = (image)
-  def image:
+  def image: Parser[String] = """Image""".r
   // footer = (footer: <link ~ link>)
-  def footer:
+  def footer: Parser[String] = """\(""".r ~ link ~ """\*""".r ~ """\)""".r ^^ {_.toString()}
   // navbar = (navbar: <link*>) // ToDo: Überlegen, wie wir umsetzen dass Links auch "Unterlinks" haben können in der Navbar.. zwei Linkarten oder als optionales Attribut hinzufügen oder ganz anders?!
-  def navbar:
+  def navbar: Parser[String] = """\(""".r ~ link ~ """\*""".r ~ """\)""".r ^^ {_.toString()}
   // header = (header: <image ~ navbar>) // CAVE: Tatsächlich ist der header leer, logo befindet sich im Body.. aber sollte ja trotzdem gleich aussehen und irrelevant sein am Ende
-  def header:
+  def header: Parser[String] = """\(""".r ~ image ~ navbar ~ """\)""".r ^^ {_.toString()}
   // body = (body: <image* ~ text* ~ list* ~ icon* ~ table* ~ formular* ~ link*>)
-  def body:
+  def body: Parser[String] = """\(""".r ~ image ~ """\*""".r ~ text ~ """\*""".r ~ icon ~ """\*""".r ~ table ~ """\*""".r ~ formular ~ """\*""".r ~ link ~ """\*""".r ~ """\)""".r ^^ {_.toString()}
   // page = (page: <header ~ body ~ footer)
-  def page:
+  def page: Parser[String] = """\(""".r ~ header ~ body ~ footer ~ """\)""".r ^^ {_.toString()}
   // website = (website: <page*>)
-  def website:
-*/
+  def website: Parser[String] = """\(""".r ~ page ~ """\*""".r ~ """\)""".r ^^ {_.toString()}
 
 }
