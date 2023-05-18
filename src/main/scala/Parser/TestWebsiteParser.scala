f1d17575a34511ca99238c1b3d5f54903c14dceb
Madiena <madelainehestermann@yahoo.de>
2023-05-18 13:42:30 +0200

    Add navbar

--- src/main/scala/Parser/TestWebsiteParser.scala
+++ src/main/scala/Parser/TestWebsiteParser.scala
@@ -2,7 +2,7 @@ package Parser
 
 object TestWebsiteParser extends WebsiteParser {
   def main(args: Array[String]): Unit = {
-    parse(icon, "(http://wwww.3.org/2000/svg)") match {
+    parse(navbar, "(Navbar: (Link: (Identifier), (Destinatin.html)), (List: (Link: (Identifier), (Destinatin.html))))") match {
       case Success(matched, _) => println(matched)
       case Failure(msg, _) => println("Neee, das war nix, weil: " + msg)
       case Error(msg, _) => ("Jetzt ist aber was ganz kaputt gegangen. Das hast du gemacht: " + msg)
